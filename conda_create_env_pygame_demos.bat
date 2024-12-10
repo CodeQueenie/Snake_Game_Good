642bf25315fd3ee7ddcce19b1475242d494c3fd0
Rich Lysakowski <rich.lysakowski@gmail.com>
2024-12-10 15:31:01 -0500

    added conda environment build batch script and added VSCode .code-workspace file.

---
+++ conda_create_env_pygame_demos.bat
@@ -0,0 +1,7 @@
+
+REM Create a new conda environment named `pygame_demos` 
+conda create --name pygame_demos --channel conda-forge python=3.10 pygame 
+
+REM Activate the new environment
+conda activate pygame_demos
+
